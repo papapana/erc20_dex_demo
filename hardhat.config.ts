5073900e2e8a590415b5fe90142198b0403ec324
Vassilis Papapanagiotou <bpapapana@gmail.com>
2025-03-14 23:26:41 +0100

    Frontend v1 complete. Also deployed to sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    testnet: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
